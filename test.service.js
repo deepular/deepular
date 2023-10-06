56997995c77e6307fdcfcd316094e3189bcf53b1
marcus-sa <marcus-sa@outlook.com>
2023-10-06 14:47:45 +0200

    init commit

---
+++ test.service.js
@@ -0,0 +1,10 @@
+import { Injectable, } from '@angular/core';
+import * as i0 from "@angular/core";
+export class TestService {
+    static { this.ɵfac = function TestService_Factory(t) { return new (t || TestService)(); }; }
+    static { this.ɵprov = /*@__PURE__*/ i0.ɵɵdefineInjectable({ token: TestService, factory: TestService.ɵfac }); }
+}
+(function () { (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(TestService, [{
+        type: Injectable
+    }], null, null); })();
+//# sourceMappingURL=test.service.js.map
